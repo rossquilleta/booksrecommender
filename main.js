49f7ded55e04ef6bcfbf03c3c02d290d09801129
rossquilleta <rosanisima@gmail.com>
2016-12-06 22:42:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,13 @@ function buscador() {
 	url: "https://www.googleapis.com/books/v1/volumes?q=" + busca,
 		dataType: "json",
 		success: function(data) {
-			console.log(data)
+			for (var i = 0; i< data.items.length ; i++){
+			resultados.innerHTML += data.items[i].volumeInfo.title +"<br></h2>"
+			}
 		},
-
-		type: "GET",
+		
+	type: "GET",
+		
 	});
 }
 
