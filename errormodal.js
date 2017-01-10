c5b91f1b33e3e099bc229485da917d6cd68fd909
rossquilleta <rosanisima@gmail.com>
2017-01-10 14:39:56 +0100

    Add files via upload
    Correcciones a final del 10/01/2017

--- errormodal.js
+++ errormodal.js
@@ -11,9 +11,9 @@ function buscador() {
 
 	success: function obras(data) {
 
-		for (var i = 0; i< 9 ; i++){
+		for (var i = 0; i< 9; i++){
 
-			pos = i;
+		//	pos = i;
 
 				if (data.items[i].volumeInfo.authors == undefined) {
 					data.items[i].volumeInfo.authors= "Autor desconocido";
@@ -33,21 +33,37 @@ function buscador() {
 				//selecc.innerHTML += data.items[i].volumeInfo.authors + data.items[i].volumeInfo.title
 
 				var port = data.items[i].volumeInfo.imageLinks.smallThumbnail;
-				var isbn = data.items[i].volumeInfo.industryIdentifiers[i];
+				var isbn = data.items[i].volumeInfo.industryIdentifiers.identifiers;
+			  var poss = i;
 				idimg = "portada"+i;
 				resultados.innerHTML += "<div class='celda'>"
-					+ '<button type="button" data-toggle="modal" data-target="#myModal" id="idimg" onclick="obras">'
+					+ '<button type="button" data-toggle="modal" data-target="#myModal" id="idimg" onclick="portada(this)">'
 					+ "<div class='portadamodal'>"
 					+ '<img src="'+port+'"/>'
 					+ '</button>'
 					+ "</div>"
 
+					function portada() {
+						$('.portadamodal img').click(function() {
+	        	//	var poss  = $(this).index();
+	        		poss = this.selectedIndex;
+							selecc.innerHTML += '<h3 style="color:#5D4C62";>'
+								+ '<h2'>+data.items[poss].volumeInfo.title+'</h2>'
+								+ data.items[poss].volumeInfo.subtitle+"<br> "
+								+ data.items[poss].volumeInfo.authors + "<br>"
+								+'<img src="'+data.items[poss].volumeInfo.imageLinks.smallThumbnail+'"/>'
+	    });
+					}
+
+
 				}
+
+
 				selecc.innerHTML += '<h3 style="color:#5D4C62";>'
 					+ data.items[i].volumeInfo.title+'</h2>'
 					+ data.items[i].volumeInfo.subtitle+"<br> "
 					+ data.items[i].volumeInfo.authors + "<br>"
-					+'<img src="'+port+'"/>'
+					+'<img src="'+data.items[i].volumeInfo.imageLinks.smallThumbnail+'"/>'
 		},
 		type: "GET",
 	});
